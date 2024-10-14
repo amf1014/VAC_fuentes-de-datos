39e766c95f9e5c6e23d73acfe60c49dbd26553dd
Claudia Gijon Pigazo <cgp1017@alu.ubu.es>
2024-10-14 12:10:27 +0200

    Cargado mi conjunto de datos

---
+++ primer_script.R
@@ -0,0 +1,5 @@
+library(dplyr)
+library(tidyverse)
+library(rjson)
+
+suicidios_por_comunidades = fromJSON(file = "C:/Users/Usuario/Downloads/Primer_Cuatri_TERCERO/FUENTES DE DATOS BIOMEDICAS Y WEB SEMANTICA/Practicas/VAC_fuentes-de-datos/CONJUNTOSDEDATOS/ConjuntoDatos_SeminarioFuentes.json")
