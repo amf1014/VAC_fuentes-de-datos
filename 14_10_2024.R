c5d6bb14922efd3bafe4219f88baaf53a9abf9b0
Verónica <vbm1007@alu.ubu.es>
2024-10-14 12:10:51 +0200

    subo mi conjunto de datos

---
+++ 14_10_2024.R
@@ -0,0 +1,10 @@
+library(dplyr)
+library(tidyverse)
+library(rjson)
+
+
+
+consumo_alcohol <- fromJSON(file = "C:/Users/USUARIO/Desktop/1_PRACTICA_FUENTES/VAC_fuentes-de-datos/CONJUNTOSDEDATOS/consumo_de_alcohol.json")
+
+view(consumo_alcohol)
+
