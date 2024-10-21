7506242bf381ee661e46940848ef50c0bcbfe3a4
Verónica <vbm1007@alu.ubu.es>
2024-10-21 10:24:58 +0200

    Actualizo

--- 14_10_2024.R
+++ 14_10_2024.R
@@ -1,10 +1,42 @@
 library(dplyr)
 library(tidyverse)
 library(rjson)
+library(tidyjson)
 
 
 
 consumo_alcohol <- fromJSON(file = "C:/Users/USUARIO/Desktop/1_PRACTICA_FUENTES/VAC_fuentes-de-datos/CONJUNTOSDEDATOS/consumo_de_alcohol.json")
 
 view(consumo_alcohol)
+
+
+head(consumo_alcohol)
+spread_all(consumo_alcohol)
+
+consumo_alcohol %>%
+  spread_all() %>% 
+  View()
+
+consumo_alcohol <- spread_all(consumo_alcohol)
+
+consumo_alcohol %>% 
+  spread_all() %>% 
+  gather_object %>% 
+  json_types %>% 
+  count(name, type)
+
+consumo_alcohol %>%
+  enter_object(Data) %>%
+  gather_array %>%
+  spread_all %>%
+  #hacer un join para unir tablas
+view(consumo_alcohol)
+
+consumo_alcohol %>%
+  enter_object(MetaData) %>%
+  gather_array %>%
+  spread_all %>%
+  #hacer un join para unir las tablas 
+view(consumo_alcohol)
+
 
