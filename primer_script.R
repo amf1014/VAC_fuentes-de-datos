9d83896b5dc5bd296eef1aa395725d2597263612
Claudia Gijon Pigazo <cgp1017@alu.ubu.es>
2024-10-21 11:26:18 +0200

    Actualizo mi script

--- primer_script.R
+++ primer_script.R
@@ -15,3 +15,21 @@ suicidio %>%
   View()
 
 suicidio <- spread_all(suicidio)
+colnames(suicidio) <- make.unique(colnames(suicidio))
+
+suicidio %>%
+  gather_object %>%
+  json_types %>%
+  count(name,type)
+
+suicidio %>%
+  enter_object(Data) %>%
+  gather_array %>%
+  spread_all 
+
+suicidio %>%
+  enter_object(MetaData) %>%
+  gather_array %>%
+  spread_all %>%
+  view()
+
