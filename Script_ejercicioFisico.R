c029b3b55004f50341340b4a2bbfd60f307b4293
amf1014 <amf1014@alu.ubu.es>
2024-11-04 10:21:18 +0100

    Trabajo en mi conjunto de datos

--- Script_ejercicioFisico.R
+++ Script_ejercicioFisico.R
@@ -1,1 +1,37 @@
 #Script de base: número de días por semana de ejercicio físico durante el tiempo de ocio según sexo y comunidad autónoma. Población de 15 y más años
+
+library(dplyr)
+library(tidyverse)
+library(rjson)
+library(tidyjson)
+
+ejercicioFisico = fromJSON(file = "CONJUNTOSDEDATOS/Número de días por semana de ejercicio físico durante el tiempo de ocio.json")
+
+head(ejercicioFisico)
+
+spread_all(ejercicioFisico)
+
+ejercicioFisico %>%
+  spread_all()
+
+
+ejercicioFisico%>%
+  gather_object %>%
+  json_types
+
+ejercicioFisico %>%
+  enter_object(Data) %>%
+  gather_array %>%
+  spread_all 
+
+ejercicioFisico %>%
+  enter_object(MetaData) %>%
+  gather_array %>%
+  spread_all 
+
+ejercicioFisico %>%
+  enter_object(Nombre) %>%
+  gather_object %>%
+  spread_all
+
+view(ejercicioFisico)
