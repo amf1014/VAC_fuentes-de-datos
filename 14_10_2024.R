91e12144028b604ecbaa279c959adef3b3121a98
Verónica <vbm1007@alu.ubu.es>
2024-11-06 17:44:49 +0100

    Reestructuro mis datos

--- 14_10_2024.R
+++ 14_10_2024.R
@@ -5,36 +5,39 @@ library(tidyjson)
 
 
 
-consumo_alcohol <- fromJSON(file = "CONJUNTOSDEDATOS/consumo_de_alcohol.json")
-
-#view(consumo_alcohol)
-
+consumo_alcohol <- fromJSON(file = "INPUT/DATA/consumo_de_alcohol.json")
 
 head(consumo_alcohol)
-spread_all(consumo_alcohol)
 
-
-consumo_alcohol_mejor<-consumo_alcohol %>% 
-  spread_all() %>% 
+consumo_alcohol_tipos<-consumo_alcohol %>% 
   gather_object %>% 
   json_types
 
-consumo_alcohol_<-consumo_alcohol %>% 
-  spread_all() %>% 
+
+consumo_alcohol_tiposresum<-consumo_alcohol %>%  
   gather_object %>% 
   json_types %>% 
   count(name, type)
 view(consumo_alcohol_mejor)
-view(consumo_alcohol_)
+view(consumo_alcohol_mejorr)
 
 
-consumo_alcohol %>%
+consumo_alcohol<-consumo_alcohol %>%
   enter_object(Data) %>%
   gather_array() %>%
-  spread_all() 
-#view(consumo_alcohol)
+  spread_all() %>%
+  select(-document.id, -array.index)
+view(consumo_alcohol)
+
+consumo_alcohol<-consumo_alcohol %>%
+  enter_object(MetaData) %>%
+  gather_array() %>%
+  spread_all %>%
+  select(-document.id, -array.index)
+  
+  
+view(consumo_alcohol)
+
 #hacer un join para unir tablas
-
-
 
 
