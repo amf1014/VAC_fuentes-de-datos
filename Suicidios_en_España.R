38eceeacc0e03ce88a2ac78ba9db0c33344e45c5
Claudia Gijon Pigazo <cgp1017@alu.ubu.es>
2024-11-08 10:43:26 +0100

    Creada tabla nueva

--- Suicidios_en_España.R
+++ Suicidios_en_España.R
@@ -9,26 +9,93 @@ suicidio
 
 head(suicidio)
 
-suicidio1 <- suicidio %>%
+suicidio %>%
   gather_object %>%
   json_types %>%
   count(name,type)
 
-suicidio2 <- suicidio %>%
+
+suicidio1 <- suicidio %>%
   enter_object(Data) %>%
   gather_array %>%
   spread_all %>%
-  select(-document.id, -array.index)
+  select("document.id", "Valor")
 
-View(suicidio2)
 
-suicidio3 <- suicidio %>%
+suicidio2 <- suicidio %>%
   enter_object(MetaData) %>%
   gather_array %>%
   spread_all %>%
-  select(-document.id, -array.index)
+  select("document.id", "Nombre")
   
-suicidio %>%
-  enter_object(Nombre) %>%
-  gather_object %>%
-  spread_all
+
+suicidio3 <- suicidio1 %>%
+  select(c("document.id","Valor")) %>%
+  full_join(x = .,
+            y = suicidio2 %>%
+              select(c("document.id","Nombre")),
+            by = c("document.id"))
+
+#view(suicidio3)
+
+suicidio4 <- suicidio3 %>%
+  mutate(
+    sexo = case_when(
+      Nombre == "Ambos sexos" ~ "Ambos sexos",
+      Nombre == "Hombres" ~ "Hombres",
+      Nombre == "Mujeres" ~ "Mujeres",
+      TRUE ~ as.character(NA)
+    )
+  )
+
+suicidio5 <- suicidio4 %>%
+  mutate(
+    años = case_when(
+      Nombre == "Todas las edades" ~ "Todas las edades",
+      Nombre == "Menores de 15 años" ~ "Menores de 15 años",
+      Nombre == "De 15 a 29 años" ~ "De 15 a 29 años",
+      Nombre == "De 30 a 39 años" ~ "De 30 a 39 años",
+      Nombre == "De 40 a 44 años" ~ "De 40 a 44 años",
+      Nombre == "De 45 a 49 años" ~ "De 45 a 49 años",
+      Nombre == "De 50 a 54 años" ~ "De 50 a 54 años",
+      Nombre == "De 55 a 59 años" ~ "De 55 a 59 años",
+      Nombre == "De 60 a 64 años" ~ "De 60 a 64 años",
+      Nombre == "De 65 a 69 años" ~ "De 65 a 69 años",
+      Nombre == "De 70 a 74 años" ~ "De 70 a 74 años",
+      Nombre == "De 75 a 79 años" ~ "De 75 a 79 años",
+      Nombre == "De 80 a 84 años" ~ "De 80 a 84 años",
+      Nombre == "De 85 a 89 años" ~ "De 85 a 89 años",
+      Nombre == "De 90 a 94 años" ~ "De 90 a 94 años",
+      Nombre == "De 95 años y más" ~ "De 95 años y más",
+      TRUE ~ as.character(NA)
+    )
+  )
+
+suicidio6 <- suicidio5 %>%
+  mutate(
+    comunidades_autonomas = case_when(
+      Nombre == "Total" ~ "Total nacional",
+      Nombre == "Andalucía" ~ "Andalucía",
+      Nombre == "Aragón" ~ "Aragón",
+      Nombre == "Asturias, Principado de" ~ "Principado de Asturias",
+      Nombre == "Balears, Illes" ~ "Islas Baleares",
+      Nombre == "Canarias" ~ "Islas Canarias",
+      Nombre == "Cantabria" ~ "Cantabria",
+      Nombre == "Castilla y León" ~ "Castilla y León",
+      Nombre == "Castilla-La Mancha" ~ "Castilla-La Mancha",
+      Nombre == "Cataluña" ~ "Cataluña",
+      Nombre == "Comunitat Valenciana" ~ "Comunitat Valenciana",
+      Nombre == "Extremadura" ~ "Extremadura",
+      Nombre == "Galicia" ~ "Galicia",
+      Nombre == "Madrid, Comunidad de" ~ "Madrid",
+      Nombre == "Murcia, Región de" ~ "Murcia",
+      Nombre == "Navarra, Comunidad Foral de" ~ "Navarra",
+      Nombre == "País Vasco" ~ "País Vasco",
+      Nombre == "Rioja, La" ~ "La Rioja",
+      Nombre == "Ceuta" ~ "Ceuta",
+      Nombre == "Melilla" ~ "Melilla",
+      TRUE ~ as.character(NA)
+    )
+  )
+
+view(suicidio6)
