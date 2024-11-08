532bec49401dab64dd4bb249f0aa23e5c996a6fd
Verónica <vbm1007@alu.ubu.es>
2024-11-08 11:15:13 +0100

    Tabla completa

--- 14_10_2024.R
+++ 14_10_2024.R
@@ -9,41 +9,81 @@ consumo_alcohol <- fromJSON(file = "INPUT/DATA/consumo_de_alcohol.json")
 
 head(consumo_alcohol)
 
-consumo_alcohol_tipos<-consumo_alcohol %>% 
-  gather_object %>% 
-  json_types
-
-
 consumo_alcohol_tiposresum<-consumo_alcohol %>%  
   gather_object %>% 
   json_types %>% 
   count(name, type)
-#view(consumo_alcohol_mejor)
-#view(consumo_alcohol_mejorr)
+consumo_alcohol_tiposresum
 
-consumo_alcohol_tiposresum$name[3]
+#consumo_alcohol_tiposresum$name[3]
 
-
-
-consumo_alcohol<-consumo_alcohol %>%
+consumo_alcohol1<-consumo_alcohol %>%
   enter_object(Data) %>%
-  gather_array() %>%
-  spread_all() 
-#%>%
- # select(-document.id, -array.index)
-view(consumo_alcohol)
-
-consumo_alcohol
-
-consumo_alcohol<-consumo_alcohol %>%
-  enter_object(MetaData) %>%
-  gather_array() %>%
+  gather_array %>%
   spread_all %>%
-  select(-document.id, -array.index)
+  select("document.id", "Valor")
+
+consumo_alcohol1
+
+consumo_alcohol2<-consumo_alcohol %>%
+  enter_object(MetaData) %>%
+  gather_array %>%
+  spread_all %>%
+  select("document.id", "Nombre")
   
-  
-view(consumo_alcohol)
+consumo_alcohol2
 
-#hacer un join para unir tablas
+consumo_alcohol3 <- consumo_alcohol1 %>%
+  select(c("document.id","Valor")) %>%
+  full_join(x = .,
+            y = consumo_alcohol2 %>%
+              select(c("document.id","Nombre")),
+            by = c("document.id"))
 
+view(consumo_alcohol3)
 
+consumo_alcohol4 <- consumo_alcohol3 %>%
+  mutate(
+    sexo = case_when(
+      Nombre == "Ambos sexos" ~ "Ambos sexos",
+      Nombre == "Hombres" ~ "Hombres",
+      Nombre == "Mujeres" ~ "Mujeres",
+      TRUE ~ as.character(NA)
+    )
+  )
+consumo_alcohol5 <- consumo_alcohol4 %>%
+  mutate(
+    comunidades_autonomas = case_when(
+      Nombre == "Total" ~ "Total Nacional",
+      Nombre == "Andalucía" ~ "Andalucía",
+      Nombre == "Aragón" ~ "Aragón",
+      Nombre == "Asturias (Principado de)" ~ "Principado de Asturias",
+      Nombre == "Balears (Illes)" ~ "Islas Baleares",
+      Nombre == "Canarias" ~ "Islas Canarias",
+      Nombre == "Cantabria" ~ "Cantabria",
+      Nombre == "Castilla y León" ~ "Castilla y León",
+      Nombre == "Castilla-La Mancha" ~ "Castilla-La Mancha",
+      Nombre == "Cataluña" ~ "Cataluña",
+      Nombre == "Comunitat Valenciana" ~ "Comunitat Valenciana",
+      Nombre == "Extremadura" ~ "Extremadura",
+      Nombre == "Galicia" ~ "Galicia",
+      Nombre == "Madrid (Comunidad de)" ~ "Madrid",
+      Nombre == "Murcia (Región de)" ~ "Murcia",
+      Nombre == "Navarra (Comunidad Foral de)" ~ "Navarra",
+      Nombre == "País Vasco" ~ "País Vasco",
+      Nombre == "Rioja (La)" ~ "La Rioja",
+      Nombre == "Ceuta (Ciudad Autónoma de)" ~ "Ceuta",
+      Nombre == "Melilla (Ciudad Autónoma de)" ~ "Melilla",
+      TRUE ~ as.character(NA)
+    )
+  )
+
+consumo_alcohol6 <- consumo_alcohol5 %>%
+  mutate(
+    años = case_when(
+      Nombre == "TOTAL" ~ "Total consumido",
+      Nombre == "Si ha consumido" ~ "Si ha consumido",
+      Nombre == "No ha consumido" ~ "No ha consumido",
+      TRUE ~ as.character(NA)
+    )
+  )
