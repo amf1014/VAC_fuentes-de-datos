ee8064f522f60d3efc0dd0e17dbb7bd9d2d52e5b
Verónica <vbm1007@alu.ubu.es>
2024-11-29 11:25:45 +0100

    cambio

--- 14_10_2024.R
+++ 14_10_2024.R
@@ -39,7 +39,6 @@ consumo_alcohol3 <- consumo_alcohol1 %>%
               select(c("document.id","Nombre")),
             by = c("document.id"))
 
-#view(consumo_alcohol3)
 
 consumo_alcohol4 <- consumo_alcohol3 %>%
   mutate(
@@ -80,97 +79,33 @@ consumo_alcohol5 <- consumo_alcohol4 %>%
 consumo_alcohol6 <- consumo_alcohol5 %>%
   mutate(
     Consumido = case_when(
-      Nombre == "TOTAL" ~ "Total consumido",
+      Nombre == "TOTAL" ~ "Total personas encuestadas",
       Nombre == "Sí ha consumido" ~ "Si ha consumido",
       Nombre == "No ha consumido" ~ "No ha consumido",
       Nombre == "No consta" ~ "No consta",
       TRUE ~ as.character(NA)
     )
   )
-view(consumo_alcohol6)
+
 consumo_alcohol7 <- 
   as.data.frame(apply(consumo_alcohol6, 2, function(col) col[!is.na(col)])) 
 
-view(consumo_alcohol7)
+
 
 consumo_alcohol7$Valor <- consumo_alcohol7$Valor[seq(1, length(consumo_alcohol7$Valor), by = 3 )]
 
-consumo_alcohol8 <- consumo_alcohol7 %>%
-  mutate(
-    habitantes = case_when(
-      Comunidades_autonomas == "Total Nacional" & Sexo == "Ambos sexos" ~ 48592802,
-      Comunidades_autonomas == "Total Nacional" & Sexo == "Hombres" ~ 23807546,
-      Comunidades_autonomas == "Total Nacional" & Sexo == "Mujeres" ~ 24785363,
-      Comunidades_autonomas == "Andalucía" & Sexo == "Ambos sexos" ~ 8620120,
-      Comunidades_autonomas == "Andalucía" & Sexo == "Hombres" ~ 4238717,
-      Comunidades_autonomas == "Andalucía" & Sexo == "Mujeres" ~ 4381403,
-      Comunidades_autonomas == "Aragón" & Sexo == "Ambos sexos" ~ 1348918,
-      Comunidades_autonomas == "Aragón" & Sexo == "Hombres" ~ 666949,
-      Comunidades_autonomas == "Aragón" & Sexo == "Mujeres" ~ 681969,
-      Comunidades_autonomas == "Principado de Asturias" & Sexo == "Ambos sexos" ~ 1008876,
-      Comunidades_autonomas == "Principado de Asturias" & Sexo == "Hombres" ~ 481018,
-      Comunidades_autonomas == "Principado de Asturias" & Sexo == "Mujeres" ~ 527858,
-      Comunidades_autonomas == "Islas Baleares" & Sexo == "Ambos sexos" ~ 1231487,
-      Comunidades_autonomas == "Islas Baleares" & Sexo == "Hombres" ~ 614011,
-      Comunidades_autonomas == "Islas Baleares" & Sexo == "Mujeres" ~ 617476,
-      Comunidades_autonomas == "Islas Canarias" & Sexo == "Ambos sexos" ~ 2236013,
-      Comunidades_autonomas == "Islas Canarias" & Sexo == "Hombres" ~ 1103805,
-      Comunidades_autonomas == "Islas Canarias" & Sexo == "Mujeres" ~ 1132208,
-      Comunidades_autonomas == "Cantabria" & Sexo == "Ambos sexos" ~ 591151,
-      Comunidades_autonomas == "Cantabria" & Sexo == "Hombres" ~ 286341,
-      Comunidades_autonomas == "Cantabria" & Sexo == "Mujeres" ~ 304810,
-      Comunidades_autonomas == "Castilla y León" & Sexo == "Ambos sexos" ~ 2389959,
-      Comunidades_autonomas == "Castilla y León" & Sexo == "Hombres" ~ 1175016,
-      Comunidades_autonomas == "Castilla y León" & Sexo == "Mujeres" ~ 1214943,
-      Comunidades_autonomas == "Castilla-La Mancha" & Sexo == "Ambos sexos" ~ 2100523,
-      Comunidades_autonomas == "Castilla-La Mancha" & Sexo == "Hombres" ~ 1053361,
-      Comunidades_autonomas == "Castilla-La Mancha" & Sexo == "Mujeres" ~ 1047162,
-      Comunidades_autonomas == "Cataluña" & Sexo == "Ambos sexos" ~ 8021049,
-      Comunidades_autonomas == "Cataluña" & Sexo == "Hombres" ~ 3948555,
-      Comunidades_autonomas == "Cataluña" & Sexo == "Mujeres" ~ 4072494,
-      Comunidades_autonomas == "Comunitad Valenciana" & Sexo == "Ambos sexos" ~ 5316478,
-      Comunidades_autonomas == "Comunitad Valenciana" & Sexo == "Hombres" ~ 2613918,
-      Comunidades_autonomas == "Comunitad Valenciana" & Sexo == "Mujeres" ~ 2702560,
-      Comunidades_autonomas == "Extremadura" & Sexo == "Ambos sexos" ~ 1053423,
-      Comunidades_autonomas == "Extremadura" & Sexo == "Hombres" ~ 521005,
-      Comunidades_autonomas == "Extremadura" & Sexo == "Mujeres" ~ 532418,
-      Comunidades_autonomas == "Galicia" & Sexo == "Ambos sexos" ~ 2705877,
-      Comunidades_autonomas == "Galicia" & Sexo == "Hombres" ~ 1301669,
-      Comunidades_autonomas == "Galicia" & Sexo == "Mujeres" ~ 1404208,
-      Comunidades_autonomas == "Madrid" & Sexo == "Ambos sexos" ~ 7000621,
-      Comunidades_autonomas == "Madrid" & Sexo == "Hombres" ~ 3352591,
-      Comunidades_autonomas == "Madrid" & Sexo == "Mujeres" ~ 3648030,
-      Comunidades_autonomas == "Murcia" & Sexo == "Ambos sexos" ~ 1569164,
-      Comunidades_autonomas == "Murcia" & Sexo == "Hombres" ~ 786213,
-      Comunidades_autonomas == "Murcia" & Sexo == "Mujeres" ~ 782951,
-      Comunidades_autonomas == "Navarra" & Sexo == "Ambos sexos" ~ 678103,
-      Comunidades_autonomas == "Navarra" & Sexo == "Hombres" ~ 335742,
-      Comunidades_autonomas == "Navarra" & Sexo == "Mujeres" ~ 342361,
-      Comunidades_autonomas == "País Vasco" & Sexo == "Ambos sexos" ~ 2227581,
-      Comunidades_autonomas == "País Vasco" & Sexo == "Hombres" ~ 1083234,
-      Comunidades_autonomas == "País Vasco" & Sexo == "Mujeres" ~ 1144347,
-      Comunidades_autonomas == "La Rioja" & Sexo == "Ambos sexos" ~ 324226,
-      Comunidades_autonomas == "La Rioja" & Sexo == "Hombres" ~ 160074,
-      Comunidades_autonomas == "La Rioja" & Sexo == "Mujeres" ~ 164152,
-      Comunidades_autonomas == "Ceuta" & Sexo == "Ambos sexos" ~ 83284,
-      Comunidades_autonomas == "Ceuta" & Sexo == "Hombres" ~ 41990,
-      Comunidades_autonomas == "Ceuta" & Sexo == "Mujeres" ~ 41294,
-      Comunidades_autonomas == "Melilla" & Sexo == "Ambos sexos" ~ 86056,
-      Comunidades_autonomas == "Melilla" & Sexo == "Hombres" ~ 43337,
-      Comunidades_autonomas == "Melilla" & Sexo == "Mujeres" ~ 42719,
-      TRUE ~ as.numeric(NA)
-    )
-  )
-view(consumo_alcohol8)
 
-consumo_alcohol9 <- consumo_alcohol8 %>%
+#HACER UN PORCENTAJE CON LAS PERSONAS ENCUESTADAS MIRAR PIVOT_LONGER.
+
+view(consumo_alcohol7)
+consumo_alcohol8 <- consumo_alcohol7 %>%
   mutate(
     porcentaje_consumo = (as.numeric(Valor) / habitantes)
   )
-consumo_alcohol9
-#view(consumo_alcohol9)
+consumo_alcohol8
 
-consumo_alcohol9 <- consumo_alcohol9[, -3]  #Borra columna Nombre
+
+consumo_alcohol8 <- consumo_alcohol8[, -3]  #Borra columna Nombre
 
 consumo_por_sexo<- consumo_alcohol8 %>%
   group_by(Sexo) %>%
